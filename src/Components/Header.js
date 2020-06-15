e9925c8b4f167fcaae847c3fe549f3e4de59f2de
adelina2999 <tataru.adelina99@gmail.com>
2020-06-15 15:54:42 +0300

    Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import instagramLogo from '../Assets/logo-text.png'
 
 function Header() {
   return (
-    <div>
-      Header
+    <div className="bg-light pt-2 pb-2">
+        <div className="row">
+
+          <div className="col d-flex justify-content-center">
+            <img src={instagramLogo} alt="Logo" className="instagram-logo"/> 
+          </div>
+
+          <div className="col d-flex justify-content-center input-group-sm">
+            <input className="form-control" style={ {width: "40%" }} type="text" placeholder="Search" aria-label="Search" />
+          </div>
+
+          <div className="col d-flex justify-content-center">
+            <a className="btn btn-primary" href="#" role="button">Log In</a>
+            <button type="button" className="btn btn-link">Sign Up</button>
+          </div>
+        </div>
     </div>
   );
 }
 
 export default Header;
+  
--- src/Components/InfoBlock.js
+++ src/Components/InfoBlock.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import profilePicture from '../Assets/profile.png';
 
 function InfoBlock() {
   return (
     <div>
-      InfoBlock
+      <img src={profilePicture} alt="Profile" className="rounded-circle profile-picture"/>
     </div>
   );
 }
