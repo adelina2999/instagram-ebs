8e6d6dca81d35e493c6999f441bbae0584a10bee
adelina2999 <tataru.adelina99@gmail.com>
2020-06-16 22:10:18 +0300

    Added all InfoBlocks to one folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header';
-import InfoBlock from './Components/InfoBlock'
+import InfoBlock from './Components/Info/InfoBlock'
 import ProfilePhotos from './Components/ProfilePhotos'
-import PopUp from './Components/PopUp'
+// import PopUp from './Components/PopUp'
 
 function App() {
   return (
