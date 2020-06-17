d2cb63ba87fdc87df3407af05e5b2aa9797050dc
adelina2999 <tataru.adelina99@gmail.com>
2020-06-17 22:13:36 +0300

    Add details photo

--- src/Components/ProfilePhotos.js
+++ src/Components/ProfilePhotos.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import axios from 'axios';
+import React from 'react'
+import axios from 'axios'
+import PopUp from './PopUp'
 import CONSTANTS from '../constants'
 
 export default class ProfilePhotos extends React.Component {
   state = {
-    photosArray: []
+    photosArray: [],
+    isShown: false,
+    photoId: ""
   }
 
   componentDidMount() {
@@ -15,24 +18,32 @@ export default class ProfilePhotos extends React.Component {
       })
   }
 
+  handleClick = (photoId) => {   
+    this.setState({ photoId });
+    this.setState(state => ({ isShown: !state.isShown }));
+  }
+
+  getDataById = () =>{
+    if(this.state.photoId != ''){ 
+      return this.state.photosArray.find(photo => photo.id == this.state.photoId)
+    }
+  }
+
   render() {
+    const isShown = this.state.isShown;
     return (
       <div>
-        <div>
-          <div className="row">
-            <div className="col-2"></div>
-
-            <div className="col-8 flex-container">
-              { this.state.photosArray.map(photo => 
-                <div key={photo.id} className="">
-                  <img className="image-size" src={photo.urls.small} />
-                </div>
-              )}
-            </div>
-
-            <div className="col-3"></div>
+        <div className="popup-position">{ isShown ? <PopUp photoData={this.getDataById()} /> : <> </> }</div>
+        <div className="row">
+          <div className="col-2"></div>
+          <div className="col-8 flex-container">
+            { this.state.photosArray.map(photo => 
+              <div key={photo.id} className="">
+                <img className="image-size" src={photo.urls.small} onClick={this.handleClick.bind(this, photo.id)} />
+              </div>
+            )}
           </div>
-        </div> 
+        </div>
       </div>
     )
   }
