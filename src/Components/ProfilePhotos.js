564a408cba25d4094a5cbc70da532e939daff996
adelina2999 <tataru.adelina99@gmail.com>
2020-06-17 19:33:31 +0300

    Render photos from unsplash

--- src/Components/ProfilePhotos.js
+++ src/Components/ProfilePhotos.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
-function ProfilePhotos() {
-  return (
-    <div>
-    </div>
-  );
+export default class ProfilePhotos extends React.Component {
+  state = {
+    photosArray: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.unsplash.com/photos/?client_id=4IushdH8Es7iMCJyswS7P03PfoVYNFYkB1q2fJhiudM`)
+      .then(response => {
+        const photosArray = response.data;
+        this.setState({ photosArray });
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        { this.state.photosArray.map(photo => 
+          <div key={photo.id}>
+            <img src={photo.urls.small} />
+          </div>
+        )}
+      </div>
+        
+    )
+  }
 }
-
-export default ProfilePhotos;
